be4863b510cfbd5c090a69af44509f0d0c9bce0c
Samuel Huang <samhuang91@gmail.com>
2020-09-26 23:52:52 -0700

    Rename inject to service

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 import Route from '@ember/routing/route';
 
-import { inject } from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-  @inject store;
+  @service store;
 
   async model() {
     return this.store.findAll('rental');
